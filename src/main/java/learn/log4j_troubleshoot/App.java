1bbb9495d13defcf068aa04c757422c1ca769a85
christint <christint@gmail.com>
2021-10-08 15:23:55 -0400

    first commit

---
+++ src/main/java/learn/log4j_troubleshoot/App.java
@@ -0,0 +1,13 @@
+package learn.log4j_troubleshoot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/learn/log4j_troubleshoot/log4jTest.java
@@ -0,0 +1,14 @@
+package learn.log4j_troubleshoot;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class log4jTest {
+	public static Logger log=LogManager.getLogger(log4jTest.class.getName());
+	@Test
+	public void TC1() {
+		log.info("this is a test for log4j");
+		System.out.println("test");
+	}
+}
